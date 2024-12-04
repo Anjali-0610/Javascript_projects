f01b20f80984325552844eb239c95f8135efc98f
Anjali Verma <159179036+Anjali-0610@users.noreply.github.com>
2024-12-04 08:43:17 +0000

    1 december 2024

---
+++ 5_datatypes_and_ECMA.js
@@ -0,0 +1,18 @@
+"use strict"   //treat all js as newer version
+
+//alert(3+4)   //we are using node.js not browser therefore alert will not work
+
+//Ecma is the official standard for javascript rules
+
+//datatypes primitive
+/*1.number
+2.string
+3.bigint
+4.boolean
+5.null
+6.undefined
+7.symbol*/
+
+
+console.log(typeof undefined)
+console.log(typeof null)                //it is object while undefined is a type
