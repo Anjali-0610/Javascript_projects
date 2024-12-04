07a8458d10530a5c71341f5f26dab39db1665f06
Anjali Verma <159179036+Anjali-0610@users.noreply.github.com>
2024-12-04 08:23:45 +0000

    4_variables

---
+++ 4_variables.js
@@ -0,0 +1,14 @@
+const id=123
+let email="anjali@gmail.com"
+var pass="1567"
+city="betul"
+
+email="akki@gmail"
+pass="4567"
+city="shelo"
+
+
+let undef;
+
+console.log(email)
+console.table([email,pass,city,undef])
