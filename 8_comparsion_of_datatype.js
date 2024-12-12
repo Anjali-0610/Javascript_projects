23b1a110c9fff032de6569a329b8be050525c18d
Anjali Verma <159179036+Anjali-0610@users.noreply.github.com>
2024-12-12 19:50:01 +0000

    4 December 2024

---
+++ 8_comparsion_of_datatype.js
@@ -0,0 +1,21 @@
+console.log("2">1)                  //true
+console.log("02">1)                  //true
+
+
+
+//comparison and equality checks diffferently ,,comparison count nulll to a  number treating it as 0.
+
+console.log(null<0)                    //false
+console.log(null==0)                   //false
+console.log(null<=0)                     //true
+
+
+//
+console.log(undefined<0)           //false
+console.log(undefined==0)           //false
+console.log(undefined>0)              //false
+
+
+//double and triple equal to(== vs ===)
+console.log("1"==1)            //true
+console.log("1"===1)           //false
