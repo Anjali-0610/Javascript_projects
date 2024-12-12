01c1c470c41e8209c55678bfd64712e017b562e7
Anjali Verma <159179036+Anjali-0610@users.noreply.github.com>
2024-12-12 19:32:26 +0000

    3 December 2024

---
+++ 7_string_to_number.js
@@ -0,0 +1,19 @@
+//normal string add
+
+let s1="hello"
+let s2=" anjali"
+let s3=s1+s2
+console.log(s3)
+
+
+//string with number
+console.log("1"+2)
+console.log(1+"2")
+console.log("1"+"2")
+console.log("1"+2+2)
+console.log(1+2+"2")
+
+//type conversion with +operator
+console.log(true)
+console.log(+true)
+console.log(+" ")
