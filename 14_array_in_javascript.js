afbff3b73c10a5b1e380453b830f0d9eac58a6a6
Anjali Verma <159179036+Anjali-0610@users.noreply.github.com>
2024-12-15 22:42:07 +0000

    10 December 2024

---
+++ 14_array_in_javascript.js
@@ -0,0 +1,39 @@
+/*
+1.array is an object
+2.collection of elements/items in single variable
+3.zero based indexing
+4.array make shallow copy
+5.array are not static but cannot be changed
+*/
+
+const myarr=[0,1,2,3,3,4,5]
+const myarr1=["anjali","akshita","anushka"]
+
+myarr.push(6)
+myarr.push(7)
+myarr.pop()
+myarr.unshift(-1)
+myarr.unshift(-2)    //insertion at front
+myarr.unshift(-3)     
+myarr.shift()       //deletion at front
+
+console.log(myarr)
+console.log(myarr.includes(7))
+console.log(myarr.includes(9))
+console.log(myarr.indexOf(19))
+console.log(myarr.indexOf(3))
+
+const mynew=myarr.join()        //converts the array 
+console.log(mynew)
+
+//slice and splice
+console.log("A",myarr)
+const my1=myarr.slice(1,3)
+console.log("B",myarr)
+console.log(my1)
+
+const my2=myarr.splice(1,3)
+console.log("C",myarr)
+console.log(my2)
+/*the difference between splice and slice is that,,,,, splice remove the ranged value from the original array and the upper limit is inclusive in this
+*/
