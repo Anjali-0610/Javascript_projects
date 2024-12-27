80330a1feea280df7ed3da6fbc3fdec7b18fb725
Anjali Verma <159179036+Anjali-0610@users.noreply.github.com>
2024-12-27 17:37:26 +0000

    19 December 2024(1)

--- 23_this_or_arrow_function.js
+++ 23_this_or_arrow_function.js
@@ -19,3 +19,44 @@ user.welcomemessage()
 //have standalone engines like dino and node.
 //and the global object in browser is window but in case of node it is empty
 console.log(this)
+
+//this keyword does not work inside function
+function one(){
+    let username="anjali"
+    console.log(this.username)         //->will not work and igve undefined output
+    console.log(this)
+}
+   
+one()
+
+
+//++++++++++++++++++++++++++++++++Arrow function+++++++++++++++++//
+const two0=function(){
+    let username="anjali"
+    console.log(this.username)
+    console.log(this)
+}
+two0()
+
+const two=()=>{
+    let username="anjali"
+    console.log(this.username)
+    console.log(this)               //will give empty paraenthesis and this is the diff between arraow function and normal function
+    console.log("--------------------------")
+}
+two()
+
+const addTwo=(n1,n2)=>{
+    return n1+n2
+}
+
+//implict return mein curly bracket{} use nahi hota hai esliye return keyword ke bina bhi return ho jayega
+const multiTwo=(num1,num2)=> num1*num2
+const multiTwo1=(num1,num2)=> (num1*num2)
+const multithree=(n1,n2)=>{n1}
+console.log(multithree())         //undefined kyunki curly bracket ke andar return nahi ho sakta
+
+
+//to return object we will need to enclose it within parenthesis()
+const obj=()=>({username:"anjali"})
+console.log(obj())
