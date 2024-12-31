49919a867e1d8fd79915b779674b8a54af98d916
Anjali Verma <159179036+Anjali-0610@users.noreply.github.com>
2024-12-31 18:51:56 +0000

    24 December 2024

---
+++ 28_while_and_doWhile.js
@@ -0,0 +1,13 @@
+let index=0
+while(index<=10)
+{
+    console.log(index)
+    index+=2
+}
+
+/*do while run atleast once*/
+let i=1
+do {
+    console.log(`2  X  ${i} = ${2*i}`)
+    i++
+} while (i<=10);
